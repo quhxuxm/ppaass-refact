1a980cec39f3775f5bc01152a4d0a5ed5e523621
Qu Hao <quhxuxm_quh@163.com>
2022-06-19 18:28:12 +0800

    init

--- udp_client.py
+++ udp_client.py
@@ -1,21 +1,22 @@
 import socket
 import socks
 
-host = "192.168.31.200"
-port = 888
+if __name__ == "__main__":
+    host = "192.168.31.200"
+    port = 888
 
-sock = socks.socksocket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.set_proxy(socks.SOCKS5, "127.0.0.1", 10081)
-sock.connect((host, port))
+    sock = socks.socksocket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.set_proxy(socks.SOCKS5, "127.0.0.1", 10081)
+    sock.connect((host, port))
 
-i = 0
+    i = 0
 
-while True:
-    message = bytes('This is the message.  It will be repeated: %s' % i, 'utf-8')
-    i = i + 1
-    print('UDP CLIENT: sending [%s]' % message)
-    sock.send(message)
-    data = sock.recv(65535)
-    print('UDP CLIENT: received [%s]' % data)
+    while True:
+        message = bytes('This is the message.  It will be repeated: %s' % i, 'utf-8')
+        i = i + 1
+        print('UDP CLIENT: sending [%s]' % message)
+        sock.send(message)
+        data = sock.recv(65535)
+        print('UDP CLIENT: received [%s]' % data)
 
-sock.close()
+    sock.close()
--- udp_server.py
+++ udp_server.py
@@ -1,18 +1,19 @@
 import socket
 
-host = "192.168.31.200"
-port = 888
+if __name__ == "__main__":
+    host = "192.168.31.200"
+    port = 888
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((host, port))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((host, port))
 
-i = 0
+    i = 0
 
-while True:
-    message, client_address = sock.recvfrom(65535)
-    print('UDP SERVER: connection from', client_address)
-    print('UDP SERVER: received "%s"' % message)
-    print('UDP SERVER: sending data back to the client [%s]' % i)
-    sock.sendto(bytes("UDP SERVER: server echo: [%s]" % i, "utf-8"), client_address)
-    print('UDP SERVER: finish', client_address)
-    i = i + 1
+    while True:
+        message, client_address = sock.recvfrom(65535)
+        print('UDP SERVER: connection from', client_address)
+        print('UDP SERVER: received "%s"' % message)
+        print('UDP SERVER: sending data back to the client [%s]' % i)
+        sock.sendto(bytes("UDP SERVER: server echo: [%s]" % i, "utf-8"), client_address)
+        print('UDP SERVER: finish', client_address)
+        i = i + 1
