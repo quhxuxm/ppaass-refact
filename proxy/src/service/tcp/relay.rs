f02df2792a86b9e9f8ca00aa6d400a1ddcf5ef3b
Qu Hao <quhxuxm_quh@163.com>
2022-07-06 07:37:16 +0800

    init

--- proxy/src/service/tcp/relay.rs
+++ proxy/src/service/tcp/relay.rs
@@ -273,10 +273,10 @@ impl TcpRelayFlow {
         T: RsaCryptoFetcher + Send + Sync + 'static,
     {
         loop {
-            let mut target_buffer = vec![0u8; target_buffer_size];
+            let mut target_buffer = BytesMut::with_capacity(target_buffer_size);
             let source_address = agent_connect_message_source_address.clone();
             let target_address = agent_connect_message_target_address.clone();
-            match target_stream_read.read(&mut target_buffer).await {
+            match target_stream_read.read_buf(&mut target_buffer).await {
                 Err(e) => {
                     error!(
                         "Connection [{}] error happen when relay data from target to proxy, target address={:?}, source address={:?}, error: {:#?}",
