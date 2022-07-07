4d3290e395e8c1431c236fa584fc731b16acdc92
quh <quhxuxm_quh@163.com>
2022-07-07 16:52:37 +0800

    init

--- agent-ui/src-tauri/src/main.rs
+++ agent-ui/src-tauri/src/main.rs
@@ -1,7 +1,53 @@
 #![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 
+use tauri::{Manager, PhysicalSize, SystemTray};
+
 fn main() {
+    let system_tray = SystemTray::new();
     tauri::Builder::default()
+        .system_tray(system_tray)
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::CloseRequested { .. } => {
+                std::process::exit(0);
+            },
+            tauri::WindowEvent::Resized(PhysicalSize { width, height }) => {
+                if *width == 0 && *height == 0 {
+                    event.window().hide();
+                }
+            },
+            event => {
+                println!("Window event happen: {:?}", event);
+            },
+        })
+        .on_system_tray_event(|app, event| match event {
+            tauri::SystemTrayEvent::LeftClick { .. } => {
+                let main_window = app.get_window("main").unwrap();
+                if let Ok(true) = main_window.is_visible() {
+                    main_window.hide();
+                } else {
+                    main_window.show();
+                    main_window.set_focus();
+                };
+            },
+            tauri::SystemTrayEvent::RightClick { .. } => {
+                let main_window = app.get_window("main").unwrap();
+                if let Ok(true) = main_window.is_visible() {
+                    main_window.hide();
+                } else {
+                    main_window.show();
+                    main_window.set_focus();
+                };
+            },
+            tauri::SystemTrayEvent::DoubleClick { .. } => {
+                let main_window = app.get_window("main").unwrap();
+                if let Ok(true) = main_window.is_visible() {
+                    main_window.hide();
+                } else {
+                    main_window.show();
+                };
+            },
+            _ => todo!(),
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- agent-ui/src/app/app.component.ts
+++ agent-ui/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'agent-ui';
+export class AppComponent implements OnInit {
+  public userToken: string;
+  public  proxyServerAddresses: string;
+  public enableCompressing: boolean;
+  constructor(){
+    this.userToken="";
+    this.proxyServerAddresses="";
+    this.enableCompressing=false;
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
